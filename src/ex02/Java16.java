5d287a9307fd3ec81c64d9a2db75c8d2f25dd4cb
inw-iwabuchi <70045624+inw-iwabuchi@users.noreply.github.com>
2020-11-28 21:36:01 +0900

    Java16 commit

---
+++ src/ex02/Java16.java
@@ -0,0 +1,20 @@
+package ex02;
+
+public class Java16 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		int time = 0;
+		int mile = 0;
+		System.out.println("隊長");
+		System.out.println("出発進行！");
+		while (mile < 500) {
+			time = time + 1;
+			mile = mile + 22;
+			System.out.println("隊長:");
+			System.out.println(time + "時間たったよ");
+		}
+		System.out.println("到着！！！");
+	}
+
+}
